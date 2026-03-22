e111f7f3620127e6c1fda0a0c8c1b934a4ee5797
Bauke Westendorp <baukewestendorp@gmail.com>
2026-03-22 17:32:34 +0100

    Null "None" values when importing into database

--- app/Http/Controllers/MeasurementController.php
+++ app/Http/Controllers/MeasurementController.php
@@ -7,55 +7,54 @@ use Illuminate\Http\Request;
 
 class MeasurementController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         // Get all data as JSON
         return response()->json(Measurement::all());
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $validated = $request->validate([
-            'WEATHERDATA' => 'required|array',
+            "WEATHERDATA" => "required|array",
 
-            'WEATHERDATA.*.STN' => 'required',
-            'WEATHERDATA.*.DATE' => 'required',
-            'WEATHERDATA.*.TIME' => 'required',
-            'WEATHERDATA.*.TEMP' => 'required',
-            'WEATHERDATA.*.DEWP' => 'required',
-            'WEATHERDATA.*.STP' => 'required',
-            'WEATHERDATA.*.SLP' => 'required',
-            'WEATHERDATA.*.VISIB' => 'required',
-            'WEATHERDATA.*.WDSP' => 'required',
-            'WEATHERDATA.*.PRCP' => 'required',
-            'WEATHERDATA.*.SNDP' => 'required',
-            'WEATHERDATA.*.FRSHTT' => 'required',
-            'WEATHERDATA.*.CLDC' => 'required',
-            'WEATHERDATA.*.WNDDIR' => 'required',
+            "WEATHERDATA.*.STN" => "required",
+            "WEATHERDATA.*.DATE" => "required",
+            "WEATHERDATA.*.TIME" => "required",
+            "WEATHERDATA.*.TEMP" => "required",
+            "WEATHERDATA.*.DEWP" => "required",
+            "WEATHERDATA.*.STP" => "required",
+            "WEATHERDATA.*.SLP" => "required",
+            "WEATHERDATA.*.VISIB" => "required",
+            "WEATHERDATA.*.WDSP" => "required",
+            "WEATHERDATA.*.PRCP" => "required",
+            "WEATHERDATA.*.SNDP" => "required",
+            "WEATHERDATA.*.FRSHTT" => "required",
+            "WEATHERDATA.*.CLDC" => "required",
+            "WEATHERDATA.*.WNDDIR" => "required",
         ]);
 
-        $weather_data = $validated["WEATHERDATA"];
+        foreach ($validated["WEATHERDATA"] as $item) {
+            $sanitizedItem = array_map(fn($value) => $value === "None" ? null : $value, $item);
 
-        foreach ($weather_data as $item) {
             Measurement::create([
-                'station' => $item["STN"],
-                'date' => $item["DATE"],
-                'time' => $item["TIME"],
-                'temperature' => $item["TEMP"],
-                'dewpoint_temperature' => $item["DEWP"],
-                'air_pressure_station' => $item["STP"],
-                'air_pressure_sea_level' => $item["SLP"],
-                'visibility' => $item["VISIB"],
-                'wind_speed' => $item["WDSP"],
-                'percipation' => $item["PRCP"],
-                'snow_depth' => $item["SNDP"],
-                'conditions' => $item["FRSHTT"],
-                'cloud_cover' => $item["CLDC"],
-                'wind_direction' => $item["WNDDIR"],
+                "station" => $sanitizedItem["STN"],
+                "date" => $sanitizedItem["DATE"],
+                "time" => $sanitizedItem["TIME"],
+                "temperature" => $sanitizedItem["TEMP"],
+                "dewpoint_temperature" => $sanitizedItem["DEWP"],
+                "air_pressure_station" => $sanitizedItem["STP"],
+                "air_pressure_sea_level" => $sanitizedItem["SLP"],
+                "visibility" => $sanitizedItem["VISIB"],
+                "wind_speed" => $sanitizedItem["WDSP"],
+                "percipation" => $sanitizedItem["PRCP"],
+                "snow_depth" => $sanitizedItem["SNDP"],
+                "conditions" => $sanitizedItem["FRSHTT"],
+                "cloud_cover" => $sanitizedItem["CLDC"],
+                "wind_direction" => $sanitizedItem["WNDDIR"],
             ]);
         }
 
-        return response()->json([
-            'status' => 'success',
-        ], 201);
+        return response()->json(["status" => "success"], 201);
     }
-
 }
