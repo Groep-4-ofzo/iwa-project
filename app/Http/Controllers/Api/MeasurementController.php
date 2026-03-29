44848f4273fa790e6b7473b4b128b7c906676ff8
Riemer <55958573+RiemerHF@users.noreply.github.com>
2026-03-29 23:36:49 +0200

    Changed Measurement Get & Added Filters
    Kan nu een combinatie van de volgende filters gebruiken:
    - from (datetime)
    - to (datetime)
    - station

--- app/Http/Controllers/Api/MeasurementController.php
+++ app/Http/Controllers/Api/MeasurementController.php
@@ -1,15 +1,51 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Measurement;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class MeasurementController extends Controller
 {
-    public function index() {
-        // Get all data as JSON
-        return response()->json(Measurement::all());
+    public function index(Request $request) {
+
+        $query = Measurement::query();
+
+        if ($request->has('from')) {
+            $datetime = explode(' ', $request->from);
+            $date = $datetime[0];
+            $time = $datetime[1] ?? '00:00:00';
+
+            $query->where(function (Builder $query) use ($date, $time) {
+                $query->where('date', '>', $date)
+                    ->orWhere(function (Builder $query2) use ($date, $time) {
+                        $query2->where('date', '=', $date)
+                            ->where('time', '>=', $time);
+                    });
+            });
+        }
+
+        if ($request->has('to')) {
+            $datetime = explode(' ', $request->to);
+            $date = $datetime[0];
+            $time = $datetime[1] ?? '23:59:59';
+
+            $query->where(function (Builder $query) use ($date, $time) {
+                $query->where('date', '<', $date)
+                    ->orWhere(function (Builder $query2) use ($date, $time) {
+                        $query2->where('date', '=', $date)
+                            ->where('time', '<=', $time);
+                    });
+            });
+        }
+
+        if ($request->has('station')) {
+            $query->where('station', $request->station);
+        }
+
+        return response()->json($query->get());
     }
 
     public function store(Request $request) {
