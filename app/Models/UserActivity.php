4cf6c4bcff27532b24c365f426fb6df9d5cb6e44
Pixel5908 <ruben.thie@hotmail.com>
2026-03-29 16:30:20 +0200

    useractivity toegevoegd

---
+++ app/Models/UserActivity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserActivity extends Model
+{
+    //
+    protected $table = 'user_activity';
+
+    protected $fillable = [
+        'endpoint_used',
+        'files_downloaded',
+        'activity_date',
+        'activity_time',
+        'authorized',
+        'data_transferred'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userid');
+    }
+}
