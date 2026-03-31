373fbdad8d8d38e166521de69842ac231a7d6548
Pixel5908 <ruben.thie@hotmail.com>
2026-03-31 13:42:08 +0200

    All migrations, query and contract models

---
+++ app/Models/Contract.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contract extends Model
+{
+    protected $table = 'contracts';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'company_id',
+        'omschrijving',
+        'start_datum',
+        'eind_datum',
+        'url',
+    ];
+}
---
+++ database/migrations/2026_03_31_104058_create_criterium_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('criterium', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('group');
+            $table->unsignedBigInteger('operator');
+            $table->unsignedBigInteger('int_value')->nullable();
+            $table->string('string_value', 45)->nullable();
+            $table->float('float_value')->nullable();
+            $table->unsignedBigInteger('value_type');
+            $table->unsignedBigInteger('value_comparison');
+
+            $table->index('group', 'idx_Criterium_Group');
+            $table->index('operator', 'idx_Criterium_Operator');
+            $table->index('value_comparison', 'idx_Criterium_Value_Comparison');
+
+            $table->foreign('group', 'fk_Criterium_Group')
+                ->references('id')
+                ->on('criterium_group');
+            $table->foreign('operator', 'fk_Criterium_Operator')
+                ->references('id')
+                ->on('operator_type');
+            $table->foreign('value_comparison', 'fk_Criterium_Value_Comparison')
+                ->references('id')
+                ->on('comparison_operator_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('criterium');
+    }
+};
